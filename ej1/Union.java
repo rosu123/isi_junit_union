4d0e6a62e95409240c2900594a7246f6422f806d
rosu123 <rosu.perela@gmail.com>
2017-12-29 21:57:23 +0100

    añado Union.java

---
+++ ej1/Union.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+
+public class Union
+{
+	public static Vector union (Vector a, Vector b)
+	{      
+		
+		
+	    if (a == null || b == null)
+	    {
+	    	throw new NullPointerException ("Union.union");
+	    }
+	    return a;
+	}
+}
